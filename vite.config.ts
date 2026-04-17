642a5643369c88bb758f0f05422303968ecb8666
rajgadhiya1digit <rajgadhiya.1digit@gmail.com>
2026-04-17 16:37:39 +0530

    Configure for GitHub Pages deployment

--- vite.config.ts
+++ vite.config.ts
@@ -3,4 +3,9 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
+  base: "/prachha/",
+  build: {
+    outDir: "dist",
+    assetsDir: "assets",
+  },
 });
